eba6a6a6af44f426f605aaaad2d61b0b50fae6c4
Matheus <mmufato@gmail.com>
2024-08-30 04:55:15 -0300

    Description

--- src/assets/pages/Teste.jsx
+++ src/assets/pages/Teste.jsx
@@ -16,16 +16,31 @@ const Teste = () => {
                     <div className="BackBtn">
                         <IoReturnDownBack className='Back'/>
                     </div>
-                    <div className="Title">
-                        <h1>Iphone 15 Pro</h1>
-                    </div>
+                    
                 </div>
 
                 <div className="MidSection">
                     <div className="Title">
-                        <h1>Iphone 15 </h1>
+                        <h1>Iphone 15 Pro</h1>
                     </div>
+                    
+                    <div className="Description info">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tortor ut. </p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tortor ut. </p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tortor ut. </p>
+                    </div>
+
+                    <div className="Git info">
+                        
+                    </div>
+
+                    <div className="Link info">
+                        
+                    </div>
+                    
+                    
                 </div>
+
             </div>
         </section>
     )
